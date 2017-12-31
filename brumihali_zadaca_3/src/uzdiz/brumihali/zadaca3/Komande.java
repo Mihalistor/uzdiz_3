49f8be47f560b99198e3b5e20c77421901bbb269
Bruno Mihalić <bruno.mihalic@hotmail.com>
2017-12-31 15:29:14 +0100

    Promjena naziva vlastitaFunkcionalnost

--- brumihali_zadaca_3/src/uzdiz/brumihali/zadaca3/Komande.java
+++ brumihali_zadaca_3/src/uzdiz/brumihali/zadaca3/Komande.java
@@ -191,7 +191,7 @@ public class Komande {
     }
 
     public void vlastitaFunkcionalnost() {
-        System.out.println("KOMANDA VLASTITA FUNKCIONALNOST KREĆE");
+        System.out.println("KOMANDA VLASTITA FUNKCIONALNOST - CHAIN OF RESPONSIBILITY");
 
     }
 
